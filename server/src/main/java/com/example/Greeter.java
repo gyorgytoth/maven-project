f97d17f53f110c0057c54e9d6707c4335962d957
gyorgytoth <gzurrr@gmail.com>
2019-04-22 23:32:31 +0200

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,8 @@ public class Greeter {
   
   /**
    * This is freet method.
+   @return String
+   @param someone is a string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
